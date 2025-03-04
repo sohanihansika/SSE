395a6bff7186555ed0439d3e307a15fa0e6069e0
Sohani <sohani@calcey.com>
2025-03-04 08:50:28 +0530

    deleted

--- src/websocket/ws.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { WebSocketService } from './ws.service';
-
-@Module({
-  providers: [WebSocketService],
-  exports: [WebSocketService],
-})
-export class WsModule {}
